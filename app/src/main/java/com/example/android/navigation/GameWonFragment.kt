887c2c605f47a4de35c46edbfce8fd91e2f50f2b
Dan Galpin <dgalpin@google.com>
2018-09-03 17:04:06 +0800

    Step.08-Exercise-Sharing-Success
    (cherry picked from commit 77c3d49d44ffd1f678a44df741669ed0438534cd)

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -38,7 +38,20 @@ class GameWonFragment : Fragment() {
             view.findNavController().navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
         }
         var args = GameWonFragmentArgs.fromBundle(arguments!!)
-        Toast.makeText(context,"NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}",Toast.LENGTH_LONG).show()
+        Toast.makeText(context,
+                "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}",
+                Toast.LENGTH_LONG).show()
+        // TODO (01) Add setHasOptionsMenu(true)
+        // This allows onCreateOptionsMenu to be called
         return binding.root
     }
+
+
+    // TODO (02) Create getShareIntent method
+    // TODO (03) Create shareSuccess method
+    // TODO (04) Override and fill out onCreateOptionsMenu
+    // Inflate the winner_menu and set the share menu item to invisible if the activity doesn't
+    // resolve
+    // TODO (05) Override onOptionsItemSelected
+    // Call the shareSuccess method when the item id matches R.id.share
 }
