c104ce6416e5a2752668403c69f4cce1f9eea9fc
Dan Galpin <dgalpin@google.com>
2018-09-03 18:18:20 +0800

    Step.06-Exercise-Adding-the-About-Menu
    (cherry picked from commit 67ae722a99deb6a98bd0b53639eebf86ee1b6152)

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -32,6 +32,20 @@ class TitleFragment : Fragment() {
                 inflater, R.layout.fragment_title, container, false)
         binding.playButton.setOnClickListener(
             Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
+        // TODO (02) Create the new menu resource
+        // Right click on the res folder within the Android project and select New Resource File
+        // Name it overflow_menu with resource type menu.  Add an About menu item with the ID of
+        // the aboutFragment.
+        // TODO (03) Call setHasOptionsMenu(true)
+        // This tells Android that our fragment has an Options Menu, so it will call
+        // onCreateOptionsMenu
         return binding.root
     }
+
+    // TODO (04) Override onCreateOptionsMenu
+    // Use the passed-in MenuInflater to inflate the overflow_menu
+
+    // TODO (05) Override onOptionsItemSelected
+    // Return true if NavigationUI.onNavDestinationSelected returns true, else return
+    // super.onOptionsItemSelected.
 }
