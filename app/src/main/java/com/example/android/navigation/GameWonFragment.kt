e05aebf4f33976e6614aad5136290f5c71f5cba1
Dan Galpin <dgalpin@google.com>
2019-01-11 17:18:09 +0800

    Step.07.Exercise-Adding-Safe-Arguments
    (cherry picked from commit 53fc65e1232ab7289d2cdfe2d0841a8ec84fb2a3)

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -33,8 +33,12 @@ class GameWonFragment : Fragment() {
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_won, container, false)
         binding.nextMatchButton.setOnClickListener(
+            // TODO (10) Replace action ID with actionGameWonFragmentToGameFragment
+            // From GameWonFragmentDirections
             Navigation.createNavigateOnClickListener(R.id.action_gameWonFragment_to_gameFragment)
         )
+        // TODO (08) Add and show toast to get the GameWonFragmentArgs from the arguments Bundle
+        // "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}"
         return binding.root
     }
 }
